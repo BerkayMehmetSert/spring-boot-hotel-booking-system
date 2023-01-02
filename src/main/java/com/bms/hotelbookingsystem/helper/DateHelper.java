e56d04bbf8a1cad4b9e8e4db02c26cfdea72ed10
Berkay <berkaymehmetsert@gmail.com>
2023-01-02 15:28:33 +0300

    Update exception message

--- src/main/java/com/bms/hotelbookingsystem/helper/DateHelper.java
+++ src/main/java/com/bms/hotelbookingsystem/helper/DateHelper.java
@@ -1,11 +1,12 @@
 package com.bms.hotelbookingsystem.helper;
 
+import com.bms.hotelbookingsystem.helper.message.BusinessMessage;
+
 import java.time.LocalDate;
 
 public class DateHelper {
-    private static final String ILLEGAL_STATE_EXCEPTION_MESSAGE = "Utility class, cannot be instantiated";
     private DateHelper() {
-        throw new IllegalStateException(ILLEGAL_STATE_EXCEPTION_MESSAGE);
+        throw new IllegalStateException(BusinessMessage.ILLEGAL_STATE_EXCEPTION);
     }
     public static LocalDate getCurrentDate() {
         return LocalDate.now();
